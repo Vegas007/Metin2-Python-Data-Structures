36d04758526280c36780a45d31dad011972fa549
VegaS <dany.sem76@yahoo.com>
2018-02-12 07:46:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-def RegisterStructClass(variables):
-    import sys
+def RegisterStructClass(members):
+	import sys
  
-    if not isinstance(variables, str):
-        print ('Members type {} need to be as {}.'.format(type(variables), type(str)))
+    if not isinstance(members, str):
+		print ('Members type {} need to be as {}.'.format(type(members), type(str)))
 		sys.exit(1)
  
-    def __init__(self, *args):
-        splitlines = variables.split()
+	def __init__(self, *args):
+		split_lines = members.split()
 
-        if len(splitlines) <> len(args):
-            print ('Failed to read arguments.')
+		if len(split_lines) <> len(args):
+			print ('Failed to read arguments.')
 			sys.exit(1)
    
-        [self.__dict__.setdefault(k, v) for (k, v) in zip(splitlines, args)]
+		[self.__dict__.setdefault(k, v) for (k, v) in zip(split_lines, args)]
  
-    return type('__struct__', (object, ), {'__init__': __init__})
+	return type('__struct__', (object, ), {'__init__': __init__})
